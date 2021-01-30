8b876c768188e79a7d5cd14601b78682815a644b
KEVIN <kevin150598@gmail.com>
2021-01-30 10:56:34 -0600

    Interfaz de usuario

--- Assets/Scripts/HealthBar/ControlesMenu.cs
+++ Assets/Scripts/HealthBar/ControlesMenu.cs
@@ -5,24 +5,28 @@ using UnityEngine.UI;
 
 public class ControlesMenu : MonoBehaviour
 {
-     [SerializeField] Text controles;
+    [SerializeField] Text controlesPerro;
+    [SerializeField] Text controlesElalebrije;
     // Start is called before the first frame update
     void Start()
     {
         
-        controles.enabled = false;
+        controlesPerro.enabled = false;
+        controlesElalebrije.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.C)){
-            Debug.Log(controles.enabled);
-            controles.enabled = true;
+            Debug.Log(controlesPerro.enabled);
+            controlesPerro.enabled = true;
+            controlesElalebrije.enabled = true;
         }
         
         if(Input.GetKeyUp(KeyCode.C)){
-            controles.enabled = false;
+            controlesPerro.enabled = false;
+            controlesElalebrije.enabled = false;
         }
     }
 }
