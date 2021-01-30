06d8da7fe1800ec20dd361e1e7d1c8f92bf055a1
arizart <20313041+arizart@users.noreply.github.com>
2021-01-29 21:08:47 -0600

    player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // player movement speed
+    [SerializeField]    
+    private float _speed = 3.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get user input (horizontal)
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // get user input (vertical)
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        // move player
+        transform.Translate(direction * _speed * Time.deltaTime);
+    }
+}
